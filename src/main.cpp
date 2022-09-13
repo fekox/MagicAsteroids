5648c72de98842ac14377be92cbca93ef385a42f
unknown <facusantos123@gmail.com>
2022-09-13 12:45:25 -0300

    Modificacion de la pestaña dentro del main

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    // Init
+    //----------------------------------------------------------------------------------
+
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
     InitWindow(screenWidth, screenHeight, "Asteroids_FacundoSantos");
 
@@ -11,16 +14,14 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", GetScreenWidth() /2.5, GetScreenHeight() /2, 20, WHITE);
 
         EndDrawing();
     }
