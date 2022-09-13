3820ffac2364819da61af2a3d7c9a0bf54370976
unknown <facusantos123@gmail.com>
2022-09-13 11:57:53 -0300

    Se integro la libreria correctamente

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Asteroids_FacundoSantos");
+
+    while (!WindowShouldClose())
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();       
 }
+
 
